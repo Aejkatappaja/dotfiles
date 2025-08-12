3f774246143e3c5d0cdf3e8956b0880df11e9b57
Aejkatappaja <joverjlj@gmail.com>
2025-08-12 23:55:03 +0200

    refactor(nvim_plugins_colorscheme): vague

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      style = "night",
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   priority = 1000,
+  --   lazy = false,
+  --   opts = {
+  --     style = "night",
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
   -- {
   --   "sainnhe/gruvbox-material",
   --   priority = 1000,
@@ -27,45 +27,48 @@ return {
   --     -- vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
   --   end,
   -- },
-  -- {
-  --   {
-  --     "vague2k/vague.nvim",
-  --     lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --     priority = 1000, -- make sure to load this before all the other plugins
-  --     config = function()
-  --       -- NOTE: you do not need to call setup if you don't want to.
-  --       require("vague").setup({
-  --         -- transparent = true,
-  --         -- optional configuration here
-  --       })
-  --       vim.cmd("colorscheme vague")
-  --     end,
-  --   },
-  -- },
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("rose-pine").setup({
-        extend_background_behind_borders = true,
-        variant = "moon", -- Choisissez le variant approprié
-        disable_background = true, -- Pour la transparence
-        styles = {
-          sidebars = "transparent", -- Pour les barres latérales
-          floats = "transparent", -- Pour les fenêtres flottantes
-          bold = true,
-          italic = false,
-          transparency = true,
-        },
-      })
-    end,
+    {
+      "vague2k/vague.nvim",
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other plugins
+      config = function()
+        -- NOTE: you do not need to call setup if you don't want to.
+        require("vague").setup({
+          -- transparent = true,
+          -- optional configuration here
+          colors = {
+            floatBorder = "#252530",
+          },
+        })
+        vim.cmd("colorscheme vague")
+      end,
+    },
   },
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("rose-pine").setup({
+  --       extend_background_behind_borders = true,
+  --       variant = "moon", -- Choisissez le variant approprié
+  --       disable_background = true, -- Pour la transparence
+  --       styles = {
+  --         sidebars = "transparent", -- Pour les barres latérales
+  --         floats = "transparent", -- Pour les fenêtres flottantes
+  --         bold = true,
+  --         italic = false,
+  --         transparency = true,
+  --       },
+  --     })
+  --   end,
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "vague",
     },
   },
 }
