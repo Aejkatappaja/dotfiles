1be90a5618b29f1ba32029ef53784e09dbd347ba
Aejkatappaja <joverjlj@gmail.com>
2025-09-15 19:43:13 +0200

    refactor(nvim): update confs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,11 @@
 -- Apply custom highlights on colorscheme change.
 -- Must be declared before executing ':colorscheme'.
--- grpid = vim.api.nvim_create_augroup("custom_highlights_gruvboxmaterial", {})
--- vim.api.nvim_create_autocmd("ColorScheme", {
---   group = grpid,
---   pattern = "gruvbox-material",
---   -- floating popups
---   command = "hi NormalFloat guibg=#282828 |" .. "hi FloatBorder guibg=#282828",
--- })
-
+grpid = vim.api.nvim_create_augroup("custom_highlights_gruvboxmaterial", {})
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = grpid,
+  pattern = "gruvbox-material",
+  -- floating popups
+  command = "hi NormalFloat guibg=#1E2021 |" .. "hi FloatBorder guibg=#1E2021",
+})
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
